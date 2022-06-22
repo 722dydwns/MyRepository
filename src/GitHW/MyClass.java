9703c719bcb53bd890851e5ad0aed56162cf3c7e
YongJOoOo <we072200@naver.com>
2022-06-22 14:26:31 +0900

    My First Commit

---
+++ src/GitHW/MyClass.java
@@ -0,0 +1,10 @@
+package GitHW;
+
+public class MyClass {
+	
+	public static void main(String args[]) {
+		System.out.println("Holle Git!");
+	}
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module HelloGit {
+	requires java.se;
+}
